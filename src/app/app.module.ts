a4e5d96c1a513361b3c458a306d2cb30c328baf2
HirabukiKoichiro <h.koichiro0402@gmail.com>
2023-03-13 15:57:21 +0900

    Udemy講座で作成した社員管理アプリです

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InMemoryDataService } from './in-memory-data.service';
+import { MemberDetailComponent } from './member-detail/member-detail.component';
+import { MembersComponent } from './members/members.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MemberSearchComponent } from './member-search/member-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MembersComponent,
+    MemberDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    MemberSearchComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
